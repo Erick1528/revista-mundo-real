9860da8f17d50d0c5f1629f6f73f5bb4203242b7
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-12-12 10:50:25 +0100

    feat: mejorar estilos y estructura del nav-bar en nav-bar.blade.php

--- resources/views/livewire/nav-bar.blade.php
+++ resources/views/livewire/nav-bar.blade.php
@@ -1,14 +1,11 @@
-<nav class=" flex justify-between items-center max-w-[1200px] mx-auto p-4 h-[68px]">
+<nav class=" flex justify-between items-center max-w-[1200px] mx-auto lg:p-4 px-10 h-[68px]">
     
-    <button class=" h-9 w-9 flex justify-center items-center outline-none">
+    <button class="hover:bg-red-light h-9 w-9 flex justify-center items-center outline-none text-black hover:text-white transition-all duration-200">
         {{-- Botón de menú --}}
-        <x-menu-svg 
-            :height="'36px'"
-            :width="'36px'"
-        />
+        <x-menu-svg height="36px" width="36px" />
     </button>
 
-    <ul class=" text-[14px] font-montserrat font-medium flex gap-8 text-primary">
+    <ul class=" text-[14px] font-montserrat font-medium lg:flex gap-8 text-primary hidden">
         <li class=" h-5">
             <a class=" h-5 hover:text-dark-sage" href="#">Destinos</a>
         </li>
@@ -34,9 +31,9 @@
         </li>
     </ul>
 
-    <button class=" h-9 w-9 flex justify-center items-center outline-none">
+    <button class="hover:bg-red-light h-9 w-9 flex justify-center items-center outline-none text-black hover:text-white transition-all duration-200">
         {{-- Botón de búsqueda --}}
-        <x-search-svg />
+        <x-search-svg height="36px" width="36px" />
     </button>
 
 </nav>
