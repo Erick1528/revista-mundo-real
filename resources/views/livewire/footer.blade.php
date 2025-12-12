66f1374cfe91f99cd3fd6e57472112cfe4c0f34c
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-12-12 10:50:15 +0100

    feat: mejorar el diseño del pie de página en footer.blade.php

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,16 +1,16 @@
 <footer class="border-t border-gray-lighter">
 
-    <div class="max-w-[1200px] w-full mx-auto px-4 py-12">
-        <div class=" grid md:grid-cols-4 gap-8 mb-8">
+    <div class="max-w-[1200px] w-full mx-auto p-10 lg:px-4 py-12">
+        <div class="flex flex-col justify-center items-center sm:grid sm:grid-cols-2 sm:items-start md:grid-cols-3 lg:grid-cols-4 gap-8 mb-8">
 
-            <div class=" max-w-[268px] w-auto">
+            <div class="max-w-[268px] w-auto sm:text-left text-center sm:order-1 md:order-1 lg:order-1">
                 <h2 class=" text-primary font-serif text-2xl mb-4">Revista Mundo Real</h2>
                 <p class=" text-sm font-opensans text-gray-light">Explorando el mundo real a través de historias
                     auténticas y
                     perspectivas únicas.</p>
             </div>
 
-            <div class="">
+            <div class="sm:text-left text-center sm:order-3 md:order-2 lg:order-2">
                 <h3 class=" font-montserrat text-sm text-primary uppercase mb-4 font-semibold">Secciones</h3>
                 <ul class=" text-sm text-gray-light font-montserrat flex flex-col gap-y-2">
 
@@ -40,7 +40,7 @@
                 </ul>
             </div>
 
-            <div class="">
+            <div class="sm:text-left text-center sm:order-4 md:order-3 lg:order-3">
                 <h3 class=" font-montserrat text-sm text-primary uppercase mb-4 font-semibold">Información</h3>
                 <ul class=" text-sm text-gray-light font-montserrat flex flex-col gap-y-2">
 
@@ -62,7 +62,7 @@
                 </ul>
             </div>
 
-            <div class="">
+            <div class="sm:text-left text-center sm:order-2 md:order-4 md:col-span-3 lg:order-4 lg:col-span-1">
                 <h3 class=" font-montserrat text-sm text-primary uppercase mb-4 font-semibold">Síguenos</h3>
                 <div class=" flex gap-3">
 
@@ -85,7 +85,7 @@
         {{-- Divisor --}}
         <div class=" bg-gray-lighter w-full h-px"></div>
 
-        <p class=" pt-8 text-center text-sm font-montserrat text-gray-light">&copy; 2025 Revista Mundo Real. Todos los derechos reservados.</p>
+        <p class=" pt-8 text-center text-[12px] sm:text-sm font-montserrat text-gray-light">&copy; 2025 Revista Mundo Real. Todos los derechos reservados.</p>
     </div>
 
 </footer>
