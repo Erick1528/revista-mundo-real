961a83a399133f261faa62e06bda6cebeca8f068
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-12-14 14:15:47 +0100

    feat: agregar componentes MidArticles y LatestArticles en index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,4 +7,10 @@
     {{-- Componente con los 4 primeros artículos --}}
     <livewire:main-articles />
 
+    {{-- Componente con los siguientes 3 artículos --}}
+    <livewire:mid-articles />
+
+    {{-- Componente con los últimos 4 artículos --}}
+    <livewire:latest-articles />
+
 @endsection
