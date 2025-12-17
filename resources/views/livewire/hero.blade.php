a7aa15ab4f3e40ad99fd7f1d1a6df04fc6a0db09
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-12-17 12:37:06 +0100

    fix: eliminar borde superior en el componente héroe

--- resources/views/livewire/hero.blade.php
+++ resources/views/livewire/hero.blade.php
@@ -1,4 +1,4 @@
-<div class=" py-8 text-center border-b border-t border-gray-lighter">
+<div class=" py-8 text-center border-b border-gray-lighter">
     <h1 class=" text-6xl sm:text-7xl font-serif text-center text-primary px-6 sm:px-10 lg:px-4 text-balance font-normal">Revista Mundo Real</h1>
     <p class=" text-[12px] sm:text-sm font-montserrat text-center mt-3 text-gray-light uppercase px-4 sm:px-6 md:p-0">Conectando Culturas · Inspirando Viajes · Celebrando Tradiciones</p>
 </div>
