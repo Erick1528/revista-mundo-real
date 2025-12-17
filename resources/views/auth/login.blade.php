ca7e9bff7bba1fbcf08d479497000bd6f635cdfc
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-12-17 12:36:44 +0100

    feat: agregar controlador de inicio de sesión y vistas asociadas

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.index')
+
+@section('title', 'Inicio de Sesión')
+
+@section('hero')
+    <livewire:hero />
+@endsection
+
+@section('content')
+    <livewire:login />
+@endsection
