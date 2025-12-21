9a90fc61f12bb993a306ae86e3d0deecd544cbcf
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-12-21 22:29:26 +0100

    feat: agregar uso del controlador DashboardController en las rutas

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
