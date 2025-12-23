f43ef1d43b6c42c50bc5a5806e59c2577d170b80
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-12-23 18:18:41 +0100

    feat: agregar componente SVG para icono de añadir

---
+++ resources/views/components/add-svg.blade.php
@@ -0,0 +1,5 @@
+<svg xmlns="http://www.w3.org/2000/svg" height="{{ $height ?? '32px' }}" viewBox="0 0 24 24"
+    width="{{ $width ?? '32px' }}">
+    <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" 
+        fill="{{ $fill ?? 'currentColor' }}" />
+</svg>
