78cb72361480cf768349183e0feede4ea0054737
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-12-23 21:47:22 +0100

    feat: reorganizar rutas y agregar controlador para la creación de artículos

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
@@ -8,4 +9,8 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+// Dashboard
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+
+    // Content Creation and Management
+    Route::get('articles/create', [ArticleController::class, 'create'])->name('articles.create')->middleware('auth');
