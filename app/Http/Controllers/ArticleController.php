3e6d434cbd1c6bcbfa568ba24fc342afd79afa7e
Erick1528 <ejosuemenjivar.15@gmail.com>
2026-02-14 13:47:42 +0100

    refactor: remove unpublished article check in show method, simplifying article display logic

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ArticleController extends Controller
 {
@@ -19,10 +20,6 @@ class ArticleController extends Controller
 
     public function show(Article $article)
     {
-        if ($article->status !== 'published') {
-            abort(404);
-        }
-
         return view('show-article', compact('article'));
     }
 }
