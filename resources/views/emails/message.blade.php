e11df4bef3abcf271457517e59bbcca2c07bde87
Erick1528 <ejosuemenjivar.15@gmail.com>
2026-02-15 21:05:20 +0100

    feat: add button support to email messages for enhanced user interaction and engagement

--- resources/views/emails/message.blade.php
+++ resources/views/emails/message.blade.php
@@ -9,4 +9,11 @@
     <p style="margin: 0 0 1em 0; font-family: 'Open Sans', Arial, sans-serif; font-size: 14px; line-height: 1.6; color: #22221d;">
         {!! nl2br(e($body ?? '')) !!}
     </p>
+    @if(isset($buttonUrl) && $buttonUrl && isset($buttonText) && $buttonText)
+    <p style="margin: 1em 0 0 0;">
+        <a href="{{ $buttonUrl }}" style="display: inline-block; padding: 12px 20px; font-family: 'Montserrat', Arial, sans-serif; font-size: 14px; font-weight: 500; color: #22221d; background-color: transparent; border: 1px solid #22221d; text-decoration: none;">
+            {{ $buttonText }}
+        </a>
+    </p>
+    @endif
 @endsection
